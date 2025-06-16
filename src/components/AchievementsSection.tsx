f360905b64b20aca4de4b288e8af3ed6c51f8d7f
yd-tw <yd960528@gmail.com>
2025-06-16 22:15:03 +0800

    udpate contect description

--- src/components/AchievementsSection.tsx
+++ src/components/AchievementsSection.tsx
@@ -10,11 +10,11 @@ type Achievement = {
 };
 
 const achievementsList: Achievement[] = [
-  { metric: "APCS", value: "4", postfix: "級" },
+  { metric: "參與社群", value: "7", postfix: "個" },
   { metric: "競賽獲獎", value: "20", postfix: "+" },
-  { metric: "服務使用者", value: "1000", postfix: "+" },
+  { metric: "網站瀏覽數", value: "1100", postfix: "+" },
   { metric: "專案數量", value: "30", postfix: "+" },
-  { metric: "社群追蹤數", value: "150", postfix: "+" },
+  { metric: "社群追蹤數", value: "210", postfix: "+" },
 ];
 
 export default function AchievementsSection() {
