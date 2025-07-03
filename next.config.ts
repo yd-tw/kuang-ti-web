44d64268bb313833091c83d3eebe4b4d2fe6b82f
yd-tw <yd960528@gmail.com>
2025-07-03 15:26:35 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [new URL("https://lh3.googleusercontent.com/**")],
+  },
 };
 
 export default nextConfig;
