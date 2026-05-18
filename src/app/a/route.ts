6ce5a4d4720a21aae4e265029f7050ef83d4be62
yd-tw <151503280+yd-tw@users.noreply.github.com>
2026-05-18 13:24:47 +0800

    Update route.ts

--- src/app/a/route.ts
+++ src/app/a/route.ts
@@ -1,1 +1,5 @@
-.
+import { permanentRedirect } from "next/navigation";
+
+export default async function Redirect() {
+  permanentRedirect(`https://docs.google.com/presentation/d/1NsU-st4B2JnKsyx4682l8Wkro5xIhBKKSivRTA-3PhY/edit?usp=drivesdk`);
+}
