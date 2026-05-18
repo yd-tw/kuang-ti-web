630b03ac1a8e3adef78a7fa7de3625603147f71e
yd-tw <151503280+yd-tw@users.noreply.github.com>
2026-05-18 13:27:13 +0800

    Delete route.ts

--- src/app/a/route.ts
+++
@@ -1,5 +0,0 @@
-import { permanentRedirect } from "next/navigation";
-
-export default async function Redirect() {
-  permanentRedirect(`https://docs.google.com/presentation/d/1NsU-st4B2JnKsyx4682l8Wkro5xIhBKKSivRTA-3PhY/edit?usp=drivesdk`);
-}
