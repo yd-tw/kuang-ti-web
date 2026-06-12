23c325e6d2fbe6ab9cdf06344cfd9a606f0a2a2c
yd-tw <151503280+yd-tw@users.noreply.github.com>
2026-06-12 12:39:04 +0800

    Update page.ts

--- src/app/a/page.ts
+++ src/app/a/page.ts
@@ -1,5 +1,5 @@
 import { permanentRedirect } from "next/navigation";
 
 export default async function Redirect() {
-  permanentRedirect(`https://github.com/whitebear13579/NTOU-MagicTreasureBox/blob/main/1132%20%E5%A4%A7%E4%B8%80%E4%B8%8B%E8%AA%B2%E7%A8%8B/B570132V-%E7%A8%8B%E5%BC%8F%E8%A8%AD%E8%A8%88%E4%BA%8C%E5%AF%A6%E7%BF%92-%E6%9E%97%E8%87%B4%E5%AE%87/Finalterm/%E5%8F%83%E8%80%83%E8%A7%A3%E7%AD%94.cpp`);
+  permanentRedirect(`https://drive.google.com/file/d/1wjHdE-ks5eVP5vEB2U694z7b_lHJQ57g/view?usp=sharing`);
 }
