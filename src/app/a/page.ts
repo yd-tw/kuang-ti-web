03ceff235da7a386729b9b59ef07ddcd097119c1
yd-tw <yd960528@gmail.com>
2026-09-20 15:16:00 +0800

    feat: add posts

--- src/app/a/page.ts
+++ src/app/a/page.ts
@@ -1,5 +1,7 @@
 import { permanentRedirect } from "next/navigation";
 
 export default async function Redirect() {
-  permanentRedirect(`https://drive.google.com/file/d/1wjHdE-ks5eVP5vEB2U694z7b_lHJQ57g/view?usp=sharing`);
+  permanentRedirect(
+    `https://drive.google.com/file/d/1wjHdE-ks5eVP5vEB2U694z7b_lHJQ57g/view?usp=sharing`,
+  );
 }
